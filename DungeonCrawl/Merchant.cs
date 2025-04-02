73719647992a2ec4eb57fbde458b1213aa6e9927
Lurppino <luka.ulmanen@gmail.com>
2025-04-02 14:12:11 +0300

    trying to make merchant

---
+++ DungeonCrawl/Merchant.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonCrawl
+{
+    internal class Merchant
+    {
+        public string name;
+        public Vector2 position;
+        public char symbol;
+        public ConsoleColor color;
+        public List<Item>? inventory;
+    }
+}
